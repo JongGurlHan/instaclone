a33f0dd6ba942d67c8926eedf6550e384ef4f210
JG HAN <gojkhan13@gmail.com>
2022-05-16 15:17:18 +0900

    0516 회원가입 - 공통 응답 DTO만들기

--- src/main/java/com/jghan/instaclone/web/AuthController.java
+++ src/main/java/com/jghan/instaclone/web/AuthController.java
@@ -1,14 +1,23 @@
 package com.jghan.instaclone.web;
 
 import com.jghan.instaclone.domain.user.User;
+import com.jghan.instaclone.handler.ex.CustomValidationException;
 import com.jghan.instaclone.service.AuthService;
 import com.jghan.instaclone.web.dto.auth.SignupDto;
+import jdk.swing.interop.SwingInterOpUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor //final 필드를 DI할때 사용
 @Controller //1.IOC 2.파일을 리턴하는 컨트롤러
@@ -35,15 +44,30 @@ public class AuthController {
     }
 
     @PostMapping("/auth/signup")
-    public String signup(SignupDto signupDto){ //key=value (x-www-form-urlencoded)
-        log.info(signupDto.toString());
-        //User < - SingupDto
-        User user = signupDto.toEntity();
-        User userEntity = authService.register(user);
-        System.out.println(userEntity);
-        log.info(user.toString());
+    public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) { //key=value (x-www-form-urlencoded)
 
-        return "auth/signin";
+        //프론트 단에서 유효성 검사해도 postman 전송할 수 있으니까
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+
+            }
+            throw new CustomValidationException("유효성 검사 실패함", errorMap);
+        } else {
+            log.info(signupDto.toString());
+
+            //User < - SingupDto
+            User user = signupDto.toEntity();
+            User userEntity = authService.register(user);
+            System.out.println(userEntity);
+            log.info(user.toString());
+
+            return "auth/signin";
+        }
+
+
     }
 
 }
--- src/main/java/com/jghan/instaclone/web/dto/auth/SignupDto.java
+++ src/main/java/com/jghan/instaclone/web/dto/auth/SignupDto.java
@@ -3,11 +3,23 @@ package com.jghan.instaclone.web.dto.auth;
 import com.jghan.instaclone.domain.user.User;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class SignupDto {
+    @Size(min=2, max = 20)
+    @NotBlank
     private  String username;
+
+    @NotBlank
     private  String password;
+
+    @NotBlank
     private  String email;
+
+    @NotBlank
     private  String name;
 
     public User toEntity(){
