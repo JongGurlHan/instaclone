1a9dfef60853f4b6cb2ff75152f576faa10b6cf4
JG HAN <gojkhan13@gmail.com>
2022-05-16 16:06:36 +0900

    0516 회원가입 - 공통 응답 스크립트 설정

--- src/main/java/com/jghan/instaclone/handler/ControllerExceptionHandler.java
+++ src/main/java/com/jghan/instaclone/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.jghan.instaclone.handler;
 
 import com.jghan.instaclone.handler.ex.CustomValidationException;
+import com.jghan.instaclone.util.Script;
 import com.jghan.instaclone.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,8 +16,9 @@ public class ControllerExceptionHandler {
 
     //CMRespDto는 전역적으로 쓸거라서 제네릭으로 선언
     @ExceptionHandler(CustomValidationException.class) //RuntimeException이 발동하는 모든 Exception을 이 함수가 가로챔
-    public CMRespDto<?> validationException(CustomValidationException e){
-        return new CMRespDto<Map<String, String >>(-1, e.getMessage(), e.getErrorMap());
+//    public CMRespDto<?> validationException(CustomValidationException e){
+//        return new CMRespDto<Map<String, String >>(-1, e.getMessage(), e.getErrorMap());
+    public String validationException(CustomValidationException e){
+        return Script.back(e.getErrorMap().toString());
     }
-    //AuthController에서 터진 Exception을  validationException이 낚아채서(실행해서) e.getMessage();를 리턴함
 }
