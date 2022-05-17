d0fdb8ae53d7a71552ee36caec069da692e3ad3e
JG HAN <gojkhan13@gmail.com>
2022-05-17 15:07:45 +0900

    0517 회원정보 수정 - 화면 구성

--- src/main/java/com/jghan/instaclone/web/UserController.java
+++ src/main/java/com/jghan/instaclone/web/UserController.java
@@ -3,6 +3,7 @@ package com.jghan.instaclone.web;
 import com.jghan.instaclone.config.auth.PrincipalDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -15,8 +16,11 @@ public class UserController {
     }
 
     @GetMapping({"/user/{id}/update"})
-    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails){
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails, Model model){
         System.out.println("세션정보"+ principalDetails.getUser());
+
+        //principal: 인증주체, 인증된 사람의 오브젝트명
+        model.addAttribute("principal", principalDetails.getUser());
         return "user/update";
     }
 }
