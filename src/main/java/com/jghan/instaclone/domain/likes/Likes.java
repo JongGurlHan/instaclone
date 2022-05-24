98b0a0830d3af465e96d5cc32e86f44ad7fcf2eb
JG HAN <gojkhan13@gmail.com>
2022-05-24 14:56:30 +0900

    0524 like 작업 - likes모델만들기

---
+++ src/main/java/com/jghan/instaclone/domain/likes/Likes.java
@@ -0,0 +1,45 @@
+package com.jghan.instaclone.domain.likes;
+
+import com.jghan.instaclone.domain.image.Image;
+import com.jghan.instaclone.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "likes_uk",
+                        columnNames = {"imageId", "userId"} //한 유저가 특정 이미지를 중복으로 like할 수 없기때문에
+                )
+        }
+)
+public class Likes { //N
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name="imageId")
+    @ManyToOne //기본 패치전략은 EAGER
+    private Image image; // 1
+
+    @JoinColumn(name="userId")
+    @ManyToOne
+    private User user; //1
+
+    private LocalDateTime createDate;
+
+    @PrePersist
+    public void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+}
